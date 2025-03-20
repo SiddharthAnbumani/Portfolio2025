e2a3b515ae3b86cac79ee816c25fe167dc663263
Siddharth Anbumani <161038518+SiddharthAnbumani@users.noreply.github.com>
2025-03-20 16:15:21 +0530

    Added Responsive Design

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Project from "./pages/Project"
 import Resume from "./pages/Resume"
+import AppBarMui from "./components/AppBarMui"
 
 export default function App(){
   return (
     <div className="background-section">
       <BrowserRouter>
-      <Navbar/>
+      <AppBarMui/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/project" element={<Project/>}/>
